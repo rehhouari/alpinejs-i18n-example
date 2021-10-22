148ccca7b10ebabfc46ece0bf7f65ea05a39ec2d
Rafik El Hadi Houari <rehhouari@gmail.com>
2021-10-22 02:16:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import 'virtual:windi.css';
 import Alpine from 'alpinejs';
-import AlpineI18n from 'https://cdn.jsdelivr.net/npm/alpinejs-i18n@2.x.x/dist/module.esm.js'
+import AlpineI18n from 'https://cdn.jsdelivr.net/npm/alpinejs-i18n@2.1.0/dist/module.esm.js'
 
 Alpine.plugin(AlpineI18n)
 Alpine.start()
